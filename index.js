e846ea9048bc8dc91b737bf08bdfcdd76e90e65d
andre <andrebertonhadev@gmail.com>
2020-10-15 16:31:34 -0700

    new routes

--- index.js
+++ index.js
@@ -15,13 +15,35 @@ const server = hapi.server({
 
 const init = async () => {
     
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function(request, reply) {
-            return `<h1> My modern api </h1>`;
-        }
-    });    
+    server.route([
+        {
+            method: 'GET',
+            path: '/',
+            handler: function(request, reply) {
+                return `<h1> My modern api </h1>`;
+            }
+        },
+        {
+            method: 'GET',
+            path: '/api/v1/paintings',
+            handler: (req, reply) => {
+                return Painting.find();
+            }
+        },
+        {
+            method: 'POST',
+            path: '/api/v1/paintings',
+            handler: (req, reply) => {
+                const { name, url, techniques } = req.payload;
+                const painting = new Painting({
+                    name,
+                    url,
+                    techniques
+                });
+                return painting.save();
+            }
+        },
+    ]);    
 
     await server.start();
     console.log(`Server running at ${server.info.uri}`);
