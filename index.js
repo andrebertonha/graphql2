fd2df04e3a7f954a4f863ca41b121c955ab74dc8
andre <andrebertonhadev@gmail.com>
2020-10-15 13:00:53 -0700

    nodemon configured to automatically keeps started the api

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ const server = hapi.server({
 });
 
 const init = async () => {
+    
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function(request, reply) {
+            return `<h1> My modern api </h1>`;
+        }
+    });    
+
     await server.start();
     console.log(`Server running at ${server.info.uri}`);
 };
+
 
 init();
