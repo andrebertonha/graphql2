aef563501f8e0cfeaba05619028a73d65e6f0627
andre <andrebertonhadev@gmail.com>
2020-10-16 08:38:51 -0700

    have to solve last bug

--- graphql/PaintingType.js
+++ graphql/PaintingType.js
@@ -3,13 +3,18 @@ const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLString } = graphql;
 
 const PaintingType = new GraphQLObjectType({
+    
     name: 'Painting',
+
     fields: () => ({
+
        id: { type: GraphQLString },
        name: { type: GraphQLString },
        url: { type: GraphQLString },
-       techniques: { type: GraphQLString },
+       technique: { type: GraphQLString },
+
     })
+
 });
 
 module.exports = PaintingType;
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
-
 const PaintingType = require('./PaintingType');
+const Painting = require('./../models/Painting');
 
 const {
     GraphQLObjectType,
@@ -9,16 +9,22 @@ const {
 } = graphql;
 
 const RootQuery = new GraphQLObjectType({
+    
     name: 'RootQueryType',
+
     fields: {
+
         painting: {
+
             type: PaintingType,
             args: { id: { type: GraphQLString } },
             resolve(parent, args) {
-                // logic for serving data
+                return Painting.findById(args.id)
             }
+
         }
     }
+
 });
 
 module.exports = new GraphQLSchema({
